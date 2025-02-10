bf290d30cfd63a6ec76e6d538bed4ae53fa8f87c
nguyenphuochuyy <phuochuy10023@gmail.com>
2025-02-10 15:15:31 +0700

    hoan thanh giao dien dang ki

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Login from './pages/Login/Login';
+import Login from './pages/Login/index';
 import "./styles/reset.scss"
+import SignUp from './pages/Signup/index';
 function App() {
   return (
     <Router>
     <Routes>
       <Route path="/" element={<>Home</>} />
       <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<SignUp/>} />
     </Routes>
   </Router>
   );
